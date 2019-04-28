3ef9b0961362ca7f7b49ed4ede6cc8f0a0dd7ce1
Fernando Noleto <fernandoleto@hotmail.com>
2019-04-28 19:19:32 -0300

    Adicionando projetos

---
+++ Sistemas_Distribuidos/src/Main/Servidor.java
@@ -0,0 +1,30 @@
+package Main;
+
+import java.net.ServerSocket;
+import java.io.IOException;
+import java.util.Scanner;
+import java.net.Socket;
+
+
+public class Servidor {
+    
+
+
+    public static void main(String args []) throws IOException {
+
+        ServerSocket servidor = new ServerSocket(12345);
+        System.out.println("Porta 12345 aberta");
+
+        Socket cliente = servidor.accept();
+        System.out.println("Nova conexão com o cliente: " +cliente.getInetAddress().getHostAddress());
+
+        Scanner entrada = new Scanner(cliente.getInputStream());
+        while(entrada.hasNextLine()) {
+            System.out.println("mensagem recebida: "+entrada.nextLine());
+        }
+
+        entrada.close();
+        servidor.close();
+    
+    }
+}
